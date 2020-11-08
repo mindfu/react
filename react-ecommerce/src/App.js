219931a8511bd873f256fda8085cad2df9c65e5e
Chris Scrivo <chris.scrivo@gmail.com>
2020-11-07 21:51:29 -0500

    Integrated firebase into application.  Added form with sign-in and sign-in with Google.

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -4,13 +4,35 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component';
+import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
-function App () {
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+  render(){
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -18,6 +40,7 @@ function App () {
         </Switch>
       </div>
     )
+  }
 }
 
 export default App;
--- react-ecommerce/src/components/header/header.component.jsx
+++ react-ecommerce/src/components/header/header.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../assets/crown.svg';
-
+import { auth } from '../../firebase/firebase.utils';
 
 import './header.styles.scss';
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -16,10 +16,13 @@ const Header = () => (
       </Link>
       <Link className='option' to='/contact'>
         CONTACT
-      </Link>
-      <Link className='option' to='/signin'>
-        SIGN IN
-      </Link>
+      </Link>      
+        {
+          currentUser ?
+          <div className='option' onClick={() => auth.signOut()}> SIGN OUT </div>
+          :
+          <Link className='option' to='/signin'> SIGN IN </Link>
+        }     
     </div>
   </div>
 )
